e8ce0830ee3036d0c55b7444ef7ec7deb488320b
陈湘 <chenxiangbj@yuanfudao.com>
2024-04-10 10:17:02 +0800

    支持 gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 import UnoCSS from 'unocss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),UnoCSS()],
+  base: './',
+  plugins: [react(), UnoCSS()],
 })
